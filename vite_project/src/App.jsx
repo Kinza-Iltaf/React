75b1a34095efc45c4d40c97f0e68b8c89ac27d12
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-06-08 18:45:44 +0000

    vite and react project setting environment

---
+++ vite_project/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+function Introduction() {
+  return  (
+    <div>
+   
+  <h1>Name : Kinza Iltaf</h1>;
+  </div>
+
+  )
+}
+
+function App() {
+
+  return (
+    <div>
+       <h1>My Intro</h1>
+       <p>short intro</p>
+       <Introduction></Introduction>
+        </div> 
+         )
+}
+
+export default App
