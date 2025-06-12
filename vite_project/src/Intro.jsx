5985792c1fe303cad446e7d841852f988f7e9977
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-06-12 20:15:31 +0000

    props

--- vite_project/src/Intro.jsx
+++ vite_project/src/Intro.jsx
@@ -1,12 +1,26 @@
 import './Intro.css';
-function Intro(){
+// function Intro(props){
+//     return (
+//         <div className='Fi'>
+//             <h2>Introduction</h2>
+//             <h3>My name is {props.name}, student of software engineering.</h3>
+
+//             <h2>Aims</h2>
+//             <h3>Master the {props.aim} concepts</h3>
+//         </div>
+//     );
+// }
+
+// second way
+
+function Intro({name,aim}){
     return (
         <div className='Fi'>
             <h2>Introduction</h2>
-            <h3>My name is Kinza Iltaf, student of software engineering.</h3>
+            <h3>My name is {name}, student of software engineering.</h3>
 
             <h2>Aims</h2>
-            <h3>Master the React concepts</h3>
+            <h3>Master the {aim} concepts</h3>
         </div>
     );
 }
