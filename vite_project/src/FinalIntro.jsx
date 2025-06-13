2463dfa98c073a0783a5e9f2a6d4a4c65c80d287
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-06-13 19:22:43 +0000

    passing groups of elements to props

--- vite_project/src/FinalIntro.jsx
+++ vite_project/src/FinalIntro.jsx
@@ -2,11 +2,13 @@ import Intro from './Intro.jsx';
 
 
 function FinalIntro(){
+  let arr = ["kinza","Fatime", "Ayesha"]
+  let arr2 ={a :"kin", b : "atiqa"}
     return(
       <>
-    <Intro />
-    <Intro />
-    <Intro />
+    <Intro name="kinza" aim="React" arr= {arr}  arr2 ={arr2}/>
+    <Intro  name="kinza" aim="React" arr= {arr} />
+    <Intro  name="kinza" aim="React" arr= {arr}/>
       </>
     )
 }
--- vite_project/src/Intro.jsx
+++ vite_project/src/Intro.jsx
@@ -13,11 +13,14 @@ import './Intro.css';
 
 // second way
 
-function Intro({name,aim}){
+function Intro({name,aim,arr,arr2}){
+
     return (
         <div className='Fi'>
             <h2>Introduction</h2>
             <h3>My name is {name}, student of software engineering.</h3>
+            <h4>{arr}</h4>
+            <h5>{arr2.a}</h5>
 
             <h2>Aims</h2>
             <h3>Master the {aim} concepts</h3>
