41afc36c9f72c4e87fd1e0e7208ac4b0975b7439
Kinza Iltaf <131205828+Kinza-Iltaf@users.noreply.github.com>
2025-06-14 22:36:45 +0000

    displaying individual elements of an array

--- vite_project/src/FinalIntro.jsx
+++ vite_project/src/FinalIntro.jsx
@@ -2,13 +2,14 @@ import Intro from './Intro.jsx';
 
 
 function FinalIntro(){
-  let arr = ["kinza","Fatime", "Ayesha"]
-  let arr2 ={a :"kin", b : "atiqa"}
+  let arr = [<li>"kinza" </li>,"Fatime", "Ayesha"];
+  let arr2 = {a:"programmer", b: "coder"};
+
     return(
       <>
-    <Intro name="kinza" aim="React" arr= {arr}  arr2 ={arr2}/>
-    <Intro  name="kinza" aim="React" arr= {arr} />
-    <Intro  name="kinza" aim="React" arr= {arr}/>
+    <Intro name="kinza" aim="React" arr= {arr} arr2={arr2} />
+    <Intro  name="kinza" aim="React" arr= {arr} arr2={arr2} />
+    <Intro  name="kinza" aim="React" arr= {arr} arr2={arr2}/>
       </>
     )
 }
